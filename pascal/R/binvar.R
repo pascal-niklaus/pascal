e15ef23846aad92e0e3128ca7b46390de8b18674
pascal-niklaus <pascal.niklaus@ieu.uzh.ch>
2017-03-27 12:05:50 +0200

    Fix in binvar and example of splittxt

--- pascal/R/binvar.R
+++ pascal/R/binvar.R
@@ -8,32 +8,34 @@
 #' 
 #' @param x vector with the center of the bins
 #' 
-#' @param rho relative probability density for each bin. \code{rho} is
-#'     normalized to unity integral, i.e. \code{sum(width*rho_i) = 1}.
+#' @param frac amount of data in bin. \code{frac} is normalized to
+#'     unity sum. Note that \code{frac} is not the probability
+#'     density, which would be \code{frac/width}.
 #'
 #' @param width width of the bin
 #'
 #' @return variance of distribution
 #' 
 #' @examples
-#'     -4
-#'
+#' ## simulate data in two bins:
+#' xsim <- c(runif(10000,0,1),runif(30000,1,1.5))
+#' hist(xsim,breaks=seq(0,2,by=.1))
+#' x <- c(.5, 1.25) # center of bins
+#' w <- c(1, .5) # width of bins
+#' f <- c(1,3)   # amount of data in bins
+#' binvar(x,f,w)
+#' var(xsim) # compare to simulated data
 #' @author Pascal Niklaus \email{pascal.niklaus@@ieu.uzh.ch}
 #' @importFrom stats weighted.mean 
 #' @keywords misc, utilities
 #' @export
-binvar <- function(x, rho, width = 1) {
-    rho <- rho / (width*sum(rho))
-    xbar <- weighted.mean(x,rho)
+binvar <- function(x, frac, width = 1) {
+    if(length(width) == 1)
+        width <- rep(width,length(x))    
+    frac <- frac / sum(frac)
+    xbar <- weighted.mean(x,frac)
     intf <- function(x) {
         x^3/3 - xbar*x^2 + xbar^2 * x
     }            
-    sum(
-        sapply(
-            seq_along(x),
-            function(i) {
-                rho[i] * ( intf( x[i] + width/2 ) - intf( x[i] - width/2 ) )
-            }
-        ))
+    sum( frac / width * ( intf( x + width/2 ) - intf( x - width/2 ) ))
 }
-
